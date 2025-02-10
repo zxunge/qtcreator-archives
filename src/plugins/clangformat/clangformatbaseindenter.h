b405afeee2f62a36d3e693c273e34d21681ba9e4
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-10 12:42:56 +0800

    Add files via upload

---
+++ src/plugins/clangformat/clangformatbaseindenter.h
@@ -0,0 +1,68 @@
+// Copyright (C) 2019 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#pragma once
+
+#include <texteditor/indenter.h>
+
+#include <QLoggingCategory>
+
+namespace clang::format { struct FormatStyle; }
+
+namespace ClangFormat {
+
+Q_DECLARE_LOGGING_CATEGORY(clangIndenterLog)
+
+class ClangFormatBaseIndenter : public TextEditor::Indenter
+{
+public:
+    ClangFormatBaseIndenter(QTextDocument *doc);
+    ~ClangFormatBaseIndenter();
+
+    TextEditor::IndentationForBlock indentationForBlocks(const QVector<QTextBlock> &blocks,
+                                                         const TextEditor::TabSettings &tabSettings,
+                                                         int cursorPositionInEditor = -1) override;
+    void indent(const QTextCursor &cursor,
+                const QChar &typedChar,
+                const TextEditor::TabSettings &tabSettings,
+                int cursorPositionInEditor = -1) override;
+
+    void reindent(const QTextCursor &cursor,
+                  const TextEditor::TabSettings &tabSettings,
+                  int cursorPositionInEditor = -1) override;
+
+    void autoIndent(const QTextCursor &cursor,
+                    const TextEditor::TabSettings &tabSettings,
+                    int cursorPositionInEditor = -1) override;
+    Utils::EditOperations format(const TextEditor::RangesInLines &rangesInLines,
+                                 FormattingMode mode = FormattingMode::Forced) override;
+
+    void indentBlock(const QTextBlock &block,
+                     const QChar &typedChar,
+                     const TextEditor::TabSettings &tabSettings,
+                     int cursorPositionInEditor = -1) override;
+
+    int indentFor(const QTextBlock &block,
+                  const TextEditor::TabSettings &tabSettings,
+                  int cursorPositionInEditor = -1) override;
+
+    bool isElectricCharacter(const QChar &ch) const override;
+
+    std::optional<int> margin() const override;
+
+    const clang::format::FormatStyle &styleForFile() const;
+
+    void setOverriddenPreferences(TextEditor::ICodeStylePreferences *preferences) final;
+    void setOverriddenStyle(const clang::format::FormatStyle &style);
+
+protected:
+    virtual bool formatCodeInsteadOfIndent() const { return false; }
+    virtual bool formatWhileTyping() const { return false; }
+    virtual int lastSaveRevision() const { return 0; }
+
+private:
+    friend class ClangFormatBaseIndenterPrivate;
+    class ClangFormatBaseIndenterPrivate *d = nullptr;
+};
+
+} // namespace ClangFormat
